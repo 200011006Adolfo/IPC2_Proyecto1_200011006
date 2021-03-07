b95338c4596b2b45b1811dcff190fe00b4c4df9a
200011006Adolfo <adolfolopez7435@gmail.com>
2021-03-06 23:19:13 -0600

    Agregando las clases nodo y lstCircular

---
+++ src/nodo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Lista no ordenada
+"""
+class Nodo:
+
+    def __init__(self,datos):
+        self.dato =datos
+        self.siguiente =None
+
+    def getDato(self):
+        return self.dato
+    def setDato(self,datos):
+        self.dato=datos
